903066fee6f317b33b2e05f90cbceb7b80caee46
Jan Reher <jreher@ep1.rub.de>
2022-08-09 22:02:04 +0200

    Fixed multiple parameters on cmt script

--- scripts/cmt.sh
+++ scripts/cmt.sh
@@ -5,4 +5,4 @@ subpath=${fullpath#*Yapp}
 cdpath="/root/workarea/Yapp$subpath"
 
 echo $cdpath
-docker exec --workdir $cdpath bosscontainer bash -c "source /root/setup.sh && cmt $@"
+docker exec --workdir $cdpath bosscontainer bash -c "source /root/setup.sh && cmt $* && chown -R $(id -u):$(id -g) /root/workarea"
