5477e3dd72277bd9a052aed0ca6f47b24c8b83d0
Jan Reher <jreher@ep1.rub.de>
2022-08-10 15:27:35 +0200

    Removed unnecessary sudos Fixes #19

--- scripts/boss.exe.sh
+++ scripts/boss.exe.sh
@@ -4,4 +4,4 @@ fullpath=$(pwd)
 subpath=${fullpath#*workarea}
 cdpath="/root/workarea$subpath"
 
-sudo docker exec --workdir "$cdpath" bosscontainer bash -c "source /root/setup.sh && boss.exe $* && chown -R $(id -u):$(id -g) $cdpath"
+docker exec --workdir "$cdpath" bosscontainer bash -c "source /root/setup.sh && boss.exe $* && chown -R $(id -u):$(id -g) $cdpath"
