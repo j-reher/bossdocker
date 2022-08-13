1374b529b0b382f31ce9c200dbaa37ac5208805d
Jan Reher <jreher@ep1.rub.de>
2022-08-13 14:18:29 +0200

    Adjusted scripts to new tag names

--- scripts/interactiveboss.sh
+++ scripts/interactiveboss.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+container_version="version=${1:-latest}"
+
 fullpath=$(pwd)
 mount=${fullpath%workarea*}
 subpath=${fullpath#*workarea}
 cdpath="/root/workarea/Yapp$subpath"
 
-docker run --security-opt label=disable -it --rm -v "$mount":/root/workarea --workdir "$cdpath" --privileged jreher/boss
+docker run --security-opt label=disable -it --rm -v "$mount":/root/workarea --workdir "$cdpath" --privileged "jreher/boss:$container_version"
