61a703c0b9b472b9a9e3307eff5716e6a07b49e3
Jan Reher <j.reher@icloud.com>
2022-08-16 18:57:05 +0200

    Transferred new docker scripts to podman

--- scripts-podman/stopboss.sh
+++ scripts-podman/stopboss.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-podman stop bosscontainer
+CLEARCACHES=0
+
+SHORT=h
+LONG=clearcaches,help
+OPTS=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+
+eval set -- "$OPTS"
+
+while :
+do
+    case "$1" in
+        --clearcaches )
+            CLEARCACHES=1
+            shift
+            ;;
+        -h | --help )
+            echo "Usage: $0 [-h]"
+            echo "  -h              Show this help"
+            exit 0
+            ;;
+        -- )
+            shift;
+            break
+            ;;
+        * )
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if podman container inspect bosscontainer >& /dev/null ; then
+podman stop bosscontainer >& /dev/null && echo "Boss container was stopped successfully."
+fi
+
+VOLUMES=$(podman volume ls -qf name=cvmfs_cache_)
+if [ $CLEARCACHES == 1 ] && [ "$VOLUMES" != "" ] ; then
+    podman volume rm $VOLUMES >& /dev/null && printf 'Removed the following CVMFS cache volumes:\n%-s\n' "$VOLUMES"
+fi
