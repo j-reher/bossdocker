d59ce9622f4f028dd3547d6e91b984a96c7125f0
Jan Reher <j.reher@icloud.com>
2022-08-16 18:33:33 +0200

    Optimized EXEC scripts
    The three exec scripts now determine the CDPATH from the workarea, which
    they pull from the docker mount options, instead of relying on a
    specific path structure.

--- scripts/boss_shell.sh
+++ scripts/boss_shell.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-fullpath=$(pwd)
-subpath=${fullpath#*workarea}
-cdpath="/root/workarea$subpath"
+if ! docker container inspect bosscontainer >& /dev/null ; then
+    echo "Boss container is not running!"
+    exit 1
+fi
 
-docker exec -it --workdir "$cdpath" bosscontainer bash
+CDPATH="/root/workarea"
+
+VOLUMESTRING="$(docker inspect -f '{{ .Mounts }}' bosscontainer)"
+TEMPSTRING1=${VOLUMESTRING#*bind}
+TEMPSTRING2=${TEMPSTRING1%/root*}
+WORKAREA="$(echo "$TEMPSTRING2" | awk '{$1=$1}1')"
+
+if [ -d "$WORKAREA" ] ; then
+    CWD=$(pwd)
+    SUBDIR=${CWD#*"$WORKAREA"}
+    if [ "$SUBDIR" != "$CWD" ] ; then
+        CDPATH="/root/workarea/$SUBDIR"
+    fi
+fi
+
+docker exec -it --workdir "$CDPATH" bosscontainer bash
