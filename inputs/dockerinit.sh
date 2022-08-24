c83834a7266d25840cf65390db15765359ff3ef9
Jan Reher <j.reher@icloud.com>
2022-08-24 14:35:57 +0200

    Accidentally included change from other branch, removing.

--- inputs/dockerinit.sh
+++ inputs/dockerinit.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-TestReleaseName="$(find /root/TestRelease/TestRelease-* -maxdepth 0 -type d)"
-if [ ! -d "/root/workarea/TestRelease/$TestReleaseName" ]; then
-  echo "$TestReleaseName not found in workarea! Copying from Template..."
-  mkdir -p /root/workarea/TestRelease
-  cp -r /root/TestRelease/* /root/workarea/TestRelease/
-fi
 source /root/mount.sh
 source /root/setup.sh
 bash -i
